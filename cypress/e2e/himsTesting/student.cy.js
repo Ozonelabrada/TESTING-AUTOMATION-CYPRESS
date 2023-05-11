a4baa6e5e92fae2aa769b741fc0e12e0468d2eb5
shoonelab'x <shoonixspider@gmail.com>
2023-05-11 08:51:29 +0800

    enhancement for student access

--- cypress/e2e/himsTesting/student.cy.js
+++ cypress/e2e/himsTesting/student.cy.js
@@ -4,14 +4,11 @@ let baseUrl = 'https://hims-v2-re2zdhj37-wacanam.vercel.app';
 
 describe('HIMS | Student Access Testing', ()=>{
 
-    it('Login Testing!', ()=>{
-        //login
+    it('Signup!', ()=>{
+        //signup
         cy.visit(baseUrl)
-        cy.get("[href='/login']").click()
-        cy.title().should('eql','Home')
-        cy.get("input[name='userName']").type("ethan")
-        cy.get("input[name='password']").type("@Ustp1234")
-        cy.get("[type='submit']").click()
+        cy.get('.css-vbbqez').click()
+        // cy.get("[type='submit']").click()
     })
     it.only('Appointments!', ()=>{
         //login
@@ -24,9 +21,6 @@ describe('HIMS | Student Access Testing', ()=>{
         //access appointments
         cy.get('.css-1xhj18k > .MuiButtonBase-root').click()
         cy.get('[href="/client/appointments"] > .MuiListItem-root > .MuiListItemIcon-root').click()
-
-
     })
-
 
 })
